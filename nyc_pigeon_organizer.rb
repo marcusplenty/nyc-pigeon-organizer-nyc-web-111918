50d436dcf3a2a3cadd9c139a497ead72f357d63e
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 09:16:16 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ def nyc_pigeon_organizer(data)
   pig_stuff = {}
   data.each do |k1,v1|
     v1.each do |k2,v2|
-      v2.each do |k3,v3|
+      v2.each do |element|
+        
       end
     end
   end
