ee89aa3c6e4676261510d2c5d896b2355479d262
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 09:32:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,4 @@
-def pigeon_organizer(hash)
+def nyc_pigeon_organizer(hash)
   names = hash.values.map {|hash| hash.values}.flatten.uniq
   skeleton = hash.keys.each_with_object(Hash.new {|k,v| k[v] = []}) {|key, hash| hash[key]}
   template = names.each_with_object({}) {|name, organizer| organizer[name] = skeleton}
