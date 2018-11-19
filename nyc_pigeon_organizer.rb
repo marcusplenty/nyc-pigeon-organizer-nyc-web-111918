0d4c832978e16c165a3cd216271b46fe5dc2a2e9
capable-publisher-7225 <marcusplenty@gmail.com>
2018-11-19 09:39:05 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          full[item][:color] << bird_color.to_s
+          full[item][:color] << bc.to_s
         end 
       end 
     end 
